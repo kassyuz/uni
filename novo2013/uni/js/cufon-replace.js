066cba58f986a5e67f66cfdc9e86cf650a75a1ea
Cassio A. Moreto <kassyuz@gmail.com>
2013-08-07 21:32:15 -0300

    first commit at Uni site

---
+++ novo2013/uni/js/cufon-replace.js
@@ -0,0 +1,17 @@
+﻿Cufon.replace('h2, h3, h4, .button, .support-phone', { fontFamily: 'Segan Light', hover:true });
+Cufon.replace('.menu > li > a', { fontFamily: 'Segan Light', hover:true });
+Cufon.replace('.footer-text', { fontFamily: 'Segan Light', textShadow:'0 0 #fff', hover:true });
+
+// Cufon.replace('h2, h3, h4, .button, .support-phone', { fontFamily: 'FranKleinBook', hover:true });
+// Cufon.replace('.menu > li > a', { fontFamily: 'FranKleinBook', hover:true });
+// Cufon.replace('.footer-text', { fontFamily: 'FranKleinBook', textShadow:'0 0 #fff', hover:true });
+
+// Cufon.replace('h2, h3, h4, .button, .support-phone', { fontFamily: 'CartoGothic Std Book', hover:true });
+// Cufon.replace('.menu > li > a', { fontFamily: 'CartoGothic Std Book', hover:true });
+// Cufon.replace('.footer-text', { fontFamily: 'CartoGothic Std Book', textShadow:'0 0 #fff', hover:true });
+
+
+// Cufon.replace('h2, h3, h4, .button, .support-phone', { fontFamily: 'Candela Book', hover:true });
+// Cufon.replace('.menu > li > a', { fontFamily: 'Candela Book', hover:true });
+// Cufon.replace('.footer-text', { fontFamily: 'Candela Book', textShadow:'0 0 #fff', hover:true });
+
